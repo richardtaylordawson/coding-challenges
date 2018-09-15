3c3aca16e25f2567163721cfc35b39c5b7625e6c
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2018-09-14 20:57:23 -0600

    Complete
    Fixing weird spacing

--- palindrome/palindrome.java
+++ palindrome/palindrome.java
@@ -4,45 +4,44 @@
 import java.util.Scanner;
 
 public class Palindrome {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String palindrome = "";
+        String palindrome = "";
 
-		Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-		if(args.length == 0) {
-			System.out.print("Please enter a potential palindrome: ");
-			palindrome = input.nextLine();
-		} else {
-			for(int i = 0; i < args.length; i++) {
-				palindrome += args[i];
-			}
-		}
+        if(args.length == 0) {
+            System.out.print("Please enter a potential palindrome: ");
+            palindrome = input.nextLine();
+        } else {
+            for(int i = 0; i < args.length; i++) {
+                palindrome += args[i];
+            }
+        }
 
-		//Replace the spaces with no space using regex
-		String combinedString = palindrome.replaceAll("\\s","");
+        //Replace the spaces with no space using regex
+        String combinedString = palindrome.replaceAll("\\s","");
 
-		//Get the number of characters entered in the palindrome
-		int size = combinedString.length();
+        //Get the number of characters entered in the palindrome
+        int size = combinedString.length();
 
-		//Integer that will start at the back of the string for comparison
-		int j = size - 1;
+        //Integer that will start at the back of the string for comparison
+        int j = size - 1;
 
-		//Loop through the string
-		for(int i = 0; i < (size / 2); i ++) {
+        //Loop through the string
+        for(int i = 0; i < (size / 2); i ++) {
+            //If the two characters do not equal each other
+            if(combinedString.charAt(i) != combinedString.charAt(j)) {
+                //Print out not a palindrome and exit the program
+                System.out.println("String is not a Palindrome");
+                System.exit(1);
+            } else {
+                //Bring J closer to the center
+                j--;
+            }
+        }
 
-			//If the two characters do not equal each other
-			if(combinedString.charAt(i) != combinedString.charAt(j)) {
-				//Print out not a palindrome and exit the program
-				System.out.println("String is not a Palindrome");
-				System.exit(1);
-			} else {
-				//Bring J closer to the center
-				j--;
-			}
-		}
-
-		//Print out string is a palindrome if code gets through loop
-		System.out.println("String is a Palindrome");
-	}
+        //Print out string is a palindrome if code gets through loop
+        System.out.println("String is a Palindrome");
+    }
 }
